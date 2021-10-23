274850d3c05614be9463584d8eb729e2aaf2e93e
Abhinav Sattiraju <asattiraju@lawn-143-215-84-190.lawn.gatech.edu>
2021-10-23 02:20:52 -0400

    message

--- backend/app.py
+++ backend/app.py
@@ -26,14 +26,56 @@ ma = Marshmallow(app)
 class Class(db.Model):
     class_id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, unique = True)
-    addresses = db.relationship('Note', backref='class', lazy=True, uselist=False)
+    notes = db.relationship('Note', backref='class', lazy=True, uselist=False)
+    posts = db.relationship('Post', backref='class', lazy=True, uselist=False)
+
+    def __init__(self, name, notes, posts):
+        self.name = name
+        self.notes = notes
+        self.posts = posts
 
 class Note(db.Model):
     note_id = db.Column(db.Integer, primary_key = True)
     class_name = db.Column(db.String, db.ForeignKey('class.name'))
     lecture = db.Column(db.Integer, primary_key = True)
     text = db.Column(db.String)
-    imgs = db.Column(db.BLOB)
+    imgs = db.Column(db.PickleType)
+
+    def __init__(self, class_name, lecture, text, imgs):
+        self.class_name = class_name
+        self.lecture = lecture
+        self.text = text
+        self.imgs = imgs
+
+class Post(db.Model):
+    post_id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    text = db.Column(db.String, db.ForeignKey('class.name'))
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+class ClassSchema(ma.Schema):
+    class Meta:
+        fields = ('name')
+
+class NoteSchema(ma.Schema):
+    class Meta:
+        fields = ('class_name','lecture','text','img')
+
+class PostSchema(ma.Schema):
+    class Meta:
+        fields = ('title','text')
+
+class_schema = ClassSchema()
+classes_schema = ClassSchema(many=True)
+
+note_schema = NoteSchema()
+notes_schema = NoteSchema(many=True)
+
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
 
 
 
